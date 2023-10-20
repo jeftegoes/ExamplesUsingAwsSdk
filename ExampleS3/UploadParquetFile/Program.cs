7bccf61e47679676abea35e3fbf3d857ca3e3d36
Jefté Goes <jefte.goes.dev@outlook.com>
2023-10-19 21:26:26 -0300

    Adding new example.

---
+++ ExampleS3/UploadParquetFile/Program.cs
@@ -0,0 +1,20 @@
+﻿using Amazon;
+using Amazon.S3;
+using Amazon.S3.Transfer;
+using UploadParquetFile;
+using Parquet.Serialization;
+
+var people = new List<Person>
+{
+    new Person() { Id = 1, Name = "Jefté" },
+    new Person() { Id = 2, Name = "Brenno" },
+    new Person() { Id = 3, Name = "Bárbara" }
+};
+
+var parqueFileName = "data2.parquet";
+
+await ParquetSerializer.SerializeAsync<Person>(people, parqueFileName);
+var s3Client = new AmazonS3Client(RegionEndpoint.SAEast1);
+
+var fileTransferUtility = new TransferUtility(s3Client);
+await fileTransferUtility.UploadAsync(parqueFileName, "products-images-939645320583");
