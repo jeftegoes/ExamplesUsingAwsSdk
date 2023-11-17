b9948c88f41b48463d2fea0166b8a43e3a9c4435
Jefté Goes <jefte.goes.dev@outlook.com>
2023-11-16 23:38:06 -0300

    Adding new example.

---
+++ ExampleCloudWatchCustomMetrics/Dotnet/Program.cs
@@ -0,0 +1,87 @@
+﻿using Amazon.CloudWatch;
+using Amazon.CloudWatch.Model;
+
+const string METRIC_NAMESPACE = "MyBooksSold";
+
+const string BOOK_1 = "Capitaes da areia";
+const string BOOK_2 = "Dona Flor e Seus Dois Maridos";
+const string BOOK_3 = "Gabriela, Cravo e Canela";
+
+var cloudWatchClient = new AmazonCloudWatchClient();
+
+var utcNowMinus15 = DateTime.UtcNow.AddMinutes(-15);
+
+var putMetricDataRequest = new PutMetricDataRequest()
+{
+    Namespace = METRIC_NAMESPACE,
+    MetricData = new List<MetricDatum>()
+    {
+        new MetricDatum()
+        {
+            MetricName = BOOK_1,
+            Value = 2,
+            Unit = StandardUnit.Count,
+            TimestampUtc = utcNowMinus15.AddMinutes(1),
+        },
+        new MetricDatum()
+        {
+            MetricName = BOOK_3,
+            Value = 1,
+            Unit = StandardUnit.Count,
+            TimestampUtc = utcNowMinus15.AddMinutes(2)
+        },
+        new MetricDatum()
+        {
+            MetricName = BOOK_1,
+            Value = 3,
+            Unit = StandardUnit.Count,
+            TimestampUtc = utcNowMinus15.AddMinutes(3)
+        },
+        new MetricDatum()
+        {
+            MetricName = BOOK_2,
+            Value = 4,
+            Unit = StandardUnit.Count,
+            TimestampUtc = utcNowMinus15.AddMinutes(4)
+        },
+        new MetricDatum()
+        {
+            MetricName = BOOK_1,
+            Value = 6,
+            Unit = StandardUnit.Count,
+            TimestampUtc = utcNowMinus15.AddMinutes(5)
+        },
+        new MetricDatum()
+        {
+            MetricName = BOOK_1,
+            Value = 3,
+            Unit = StandardUnit.Count,
+            TimestampUtc = utcNowMinus15.AddMinutes(6)
+        },
+        new MetricDatum()
+        {
+            MetricName = BOOK_1,
+            Value = 5,
+            Unit = StandardUnit.Count,
+            TimestampUtc = utcNowMinus15.AddMinutes(7)
+        },
+        new MetricDatum()
+        {
+            MetricName = BOOK_3,
+            Value = 3,
+            Unit = StandardUnit.Count,
+            TimestampUtc = utcNowMinus15.AddMinutes(8)
+        },
+        new MetricDatum()
+        {
+            MetricName = BOOK_2,
+            Value = 2,
+            Unit = StandardUnit.Count,
+            TimestampUtc = utcNowMinus15.AddMinutes(9)
+        },
+    }
+};
+
+await cloudWatchClient.PutMetricDataAsync(putMetricDataRequest);
+
+Console.WriteLine("Metrics inserted with successfully.");
---
+++ ExampleCloudWatchCustomMetrics/README.md
@@ -0,0 +1,23 @@
+# 1. What is this project
+
+This is a small / simple example that shows how using AWS SDK to create Custom Metrics.
+
+| DateHour            | Book                          | Qty |
+| ------------------- | ----------------------------- | --- |
+| 16/11/2023 10:00:10 | Capitaes da areia             | 2   |
+| 16/11/2023 10:00:20 | Gabriela, Cravo e Canela      | 1   |
+| 16/11/2023 10:00:30 | Capitaes da areia             | 3   |
+| 16/11/2023 10:00:40 | Dona Flor e Seus Dois Maridos | 4   |
+| 16/11/2023 10:00:50 | Capitaes da areia             | 6   |
+| 16/11/2023 10:01:00 | Capitaes da areia             | 3   |
+| 16/11/2023 10:01:10 | Capitaes da areia             | 5   |
+| 16/11/2023 10:01:20 | Gabriela, Cravo e Canela      | 3   |
+| 16/11/2023 10:01:30 | Dona Flor e Seus Dois Maridos | 2   |
+
+# 2. Project Type Console
+
+.NET 8.0
+
+## 2.1. Result of this project
+
+![Amazon CloudWatch Custom Metrics](Images/Metrics.png)
